0f8ba2697a21d85baf33f42f0eef9a88b66c36e6
taoudi-abdelbasset <ABD.TAOUDI-ETU@etu.univh2c.ma>
2025-10-20 12:07:14 +0100

    init: config service

---
+++ cofig-service/src/main/java/com/microservice/cofig_service/CofigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.cofig_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CofigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CofigServiceApplication.class, args);
+	}
+
+}
---
+++ cofig-service/src/test/java/com/microservice/cofig_service/CofigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.cofig_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CofigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
